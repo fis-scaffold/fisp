e1a0b426e67dae080fe87d406dfe34b4bdeebc12
2betop <liaoxuezhi@icloud.com>
2016-04-06 11:39:44 +0800

    update

--- fis-conf.js
+++ fis-conf.js
@@ -1,7 +1,7 @@
 fis.require('smarty')(fis);
 
-// 可选
-// fis.set('namespace', 'your namespace');
+// 目前 fis3-smarty 要求至少设置个 namespace
+fis.set('namespace', '${namespace}');
 
 // default media is `dev`，
 fis.media('dev').match('*', {
